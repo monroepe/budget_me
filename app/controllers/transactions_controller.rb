e1b02fa09fc1a8666fc8a8e5cbeb8ca2ffbfe524
Peter <monroepe@gmail.com>
2014-09-25 20:42:25 -0400

    Added types for transactions

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,34 @@
+class TranactionsController < ApplicationController
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @budget = current_user.budgets.find(params[:budget_id])
+    @transaction = @budget.transactions.build(transaction_params)
+
+    if @transaction.save
+      redirect_to @budget, notice: 'Transaction created successfully!'
+    else
+      render "new"
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def transaction_params
+    params.require(:transaction).permit(:name, :amount, :type_id, :description)
+  end
+end
---
+++ app/models/type.rb
@@ -0,0 +1,4 @@
+class Type < ActiveRecord::Base
+
+  validates :name, presence: true
+end
