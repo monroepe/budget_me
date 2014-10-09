dbba92b1149a7db69e2a2d24eb4d185c210cd9a0
Peter <monroepe@gmail.com>
2014-10-09 11:25:52 -0400

    Fixed budget so that it works with new schema

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -15,35 +15,36 @@ class BudgetsController < ApplicationController
   end
 
   def create
-    @budget = current_user.budgets.build(budget_params)
+    @budget = Budget.new(budget_params)
+    @budget.user = current_user
 
     if @budget.save
-      redirect_to @budget, notice: 'Budget created successfully!'
+      redirect_to user_budgets_path(current_user), notice: 'Budget created successfully!'
     else
       render "new"
     end
   end
 
   def edit
-    @budget = current_user.budgets.find(params[:id])
+    @budget = current_user.budget
   end
 
   def update
-    @budget = current_user.budgets.find(params[:id])
+    @budget = current_user.budget
 
     if @budget.update(budget_params)
-      redirect_to @budget, notice: 'Budget name updated successfully!'
+      redirect_to user_budgets_path(current_user), notice: 'Budget name updated successfully!'
     else
       render "edit"
     end
   end
 
   def destroy
-    @budget = current_user.budgets.find(params[:id])
+    @budget = current_user.budget
 
     @budget.destroy
 
-    redirect_to budgets_path, notice: 'Budget destroyed successfully!'
+    redirect_to root_path, notice: 'Budget destroyed successfully!'
   end
 
   private
